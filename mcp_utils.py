1de62a19d46f84c7114b130fd6b607ea0be12796
Matthew Brett <matthew.brett@gmail.com>
2020-03-20 12:18:22 +0000

    Further refactor of config machinery

--- mcp_utils.py
+++ mcp_utils.py
@@ -20,11 +20,15 @@ def read_config(config_fname):
     with open(config_fname, 'rt') as fobj:
         res = yaml.load(fobj)
     config_path = op.abspath(op.dirname(config_fname))
-    res['canvas_export'] = op.join(config_path, res['canvas_export'])
+    for key, value in res.items():
+        if not key.endswith('_path'):
+            continue
+        if not op.isabs(value):
+            res[key] = op.join(config_path, value)
     return res
 
 
 def get_minimal_df(config):
-    df = read_canvas(config['canvas_export'])
+    df = read_canvas(config['canvas_export_path'])
     df[config['assignment_name']] = np.nan
     return df
