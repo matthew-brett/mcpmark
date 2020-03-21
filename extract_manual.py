261c574c3656a7a24b1e3eb18c2c2333f313f02a
Matthew Brett <matthew.brett@gmail.com>
2020-03-21 16:46:54 +0000

    First working draft of extract script.

--- extract_manual.py
+++ extract_manual.py
@@ -9,7 +9,7 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import jupytext
 
 from mcp_utils import (read_config, get_minimal_df, get_notebooks,
-                       loginfn2login)
+                       loginfn2login, MCPError)
 
 
 
@@ -43,30 +43,42 @@ def extract_from_nb(nb_fname, labels):
 def process_nbs(nb_fnames, ex_labels):
     slabels = set(ex_labels)
     all_answers = {}
+    errors = []
     for nb_fname in nb_fnames:
         answers = extract_from_nb(nb_fname, ex_labels)
         missing = slabels.difference(answers)
         if missing:
-            print(f'{nb_fname} has no metadata for {missing}')
-        all_answers[loginfn2login(nb_fname)] = answers
+            errors.append(f'{nb_fname} has no metadata for {missing}')
+        else:
+            all_answers[loginfn2login(nb_fname)] = answers
+    if errors:
+        raise MCPError('\n'.join(errors))
     return all_answers
 
 
-def write_answers(all_answers, config):
-    print(all_answers)
+def write_answers(all_answers, out_path):
+    k0 = list(all_answers)[0]
+    for label in all_answers[k0]:
+        lines = [f'# Answers for {label}', '']
+        for login, a_dict in all_answers.items():
+            answer = a_dict[label]
+            lines += [f'## {login}', '', f'{answer}', '', 'Score:', '']
+        out_fname = op.join(out_path, label + '.md')
+        with open(out_fname, 'wt') as fobj:
+            fobj.write('\n'.join(lines))
 
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    config = read_config(args.config_path)
     lexts = args.nb_lext if args.nb_lext else ['.rmd', '.ipynb']
     nb_fnames = get_notebooks(args.nb_path, lexts, first_only=True)
     if len(nb_fnames) == 0:
         raise RuntimeError(f'No notebooks found in path "{args.nb_path}" '
                            f'with extensions {lexts}')
     all_answers = process_nbs(nb_fnames, args.ex_labels)
-    write_answers(all_answers, config)
+    assert len(all_answers) == len(nb_fnames)
+    write_answers(all_answers, args.nb_path)
 
 
 if __name__ == '__main__':
