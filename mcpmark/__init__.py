aaab52ba7952c028af597005c8dc95f18b45bcb8
Matthew Brett <matthew.brett@gmail.com>
2020-05-01 14:21:16 +0100

    Add get_minimal_df to __init__

--- mcpmark/__init__.py
+++ mcpmark/__init__.py
@@ -4,4 +4,5 @@
 __version__ = '0.1a1'
 
 from .mcputils import (get_notebooks, get_manual_scores, MCPError,
-                       match_plot_scores, read_config, execute_nb_fname)
+                       get_minimal_df, match_plot_scores, read_config,
+                       execute_nb_fname)
