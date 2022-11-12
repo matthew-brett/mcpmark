1ecb009de9d095cb9bdaa59d6fe0b9a8b37bbc55
Matthew Brett <matthew.brett@gmail.com>
2022-11-12 20:55:37 +0000

    Allow hyphen in user names for plots

--- mcpmark/mcputils.py
+++ mcpmark/mcputils.py
@@ -301,7 +301,7 @@ def _check_total(line, name, marks, required_fields, msg_lines):
 
 
 
-NAME_RE = re.compile(r'^##\s+([a-zA-Z0-9_]+)\s*$')
+NAME_RE = re.compile(r'^##\s+([a-zA-Z0-9_-]+)\s*$')
 SCORE_RE = re.compile(r'\s*MCPScore\s*:\s*([0-9.]+)\s*$')
 QUESTION_RE = re.compile(r'^(.*)_report\.md')
 
