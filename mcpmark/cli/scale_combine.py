c5ba88dadf815f4b559729adf670c01630b9f102
Matthew Brett <matthew.brett@gmail.com>
2023-01-16 10:57:40 +0000

    Refactor component output files

--- mcpmark/cli/scale_combine.py
+++ mcpmark/cli/scale_combine.py
@@ -2,8 +2,7 @@
 """ Scale marks to final totals, combine components (if more than one).
 """
 
-import os
-import os.path as op
+from pathlib import Path
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 import pandas as pd
@@ -15,32 +14,35 @@ def get_parser():
     parser = ArgumentParser(description=__doc__,  # Usage from docstring
                             formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('--config-path',
-                        default=op.join(os.getcwd(), 'assign_config.yaml'),
+                        default=Path('assign_config.yaml'),
                         help='Path to config file')
     return parser
 
 
+def read_component(name, config):
+    csv_pth = (Path(component_path(config, name)) /
+                    'marking' /
+                    'component.csv')
+    if not csv_pth.is_file():
+        raise RuntimeError(f'No component csv file at {csv_pth}; '
+                            'Do you need to run mcp-grade-component?')
+    return pd.read_csv(csv_pth, header=[0, 1], index_col=0)
+
+
 def process_components(config):
-    series = {}
-    stid_col = config['student_id_col']
     components = config['components']
     scaled_max = sum([components[c]['scaled_to'] for c in components])
+    final = pd.DataFrame()
     for name, info in components.items():
-        csv_pth = op.join(component_path(config, name),
-                          'marking',
-                          'component.csv')
-        if not op.isfile(csv_pth):
-            raise RuntimeError(f'No component csv file at {csv_pth}; '
-                               'Do you need to run mcp-grade-component?')
-        df = pd.read_csv(csv_pth).set_index(stid_col)
-        series[name] = df['Mark'] * info['scaled_to'] / info['actual_max']
-    final = pd.DataFrame(series)
+        df = read_component(name, config)
+        final[name] = df['Total'] * info['scaled_to'] / info['actual_max']
     total = final.sum(axis=1)
     if config.get('round_final'):
         total = round(total)
     final['Percent'] = total / scaled_max * 100
     final['Total'] = total
-    return final
+    return final.reset_index().rename(
+        columns={'index': config['student_id_col']})
 
 
 def main():
@@ -48,8 +50,8 @@ def main():
     args = parser.parse_args()
     config = read_config(args.config_path)
     all_answers = process_components(config)
-    out_csv = op.join(config['base_path'], config['mark_fname'])
-    all_answers.to_csv(out_csv)
+    out_csv = Path(config['base_path']) / config['mark_fname']
+    all_answers.to_csv(out_csv, index=None)
     print(all_answers.describe())
 
 
