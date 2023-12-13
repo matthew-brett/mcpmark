bb2b50ce106c538f5fdb5e88ed09fc3ef1e5ff09
Matthew Brett <matthew.brett@gmail.com>
2023-12-13 12:56:18 +0000

    Revert accidental commit of feedback draft

--- mcpmark/cli/make_feedback.py
+++ mcpmark/cli/make_feedback.py
@@ -131,16 +131,6 @@ class ExtPath(FBPath):
         return op.join(self.root_path, uuid)
 
 
-def write_spec(component_path, spec):
-
-
-def write_data(component_path, data_specs):
-    written = []
-    for spec in data_specs:
-        written += write_spec(component_path, spec)
-    return written
-
-
 def write_component(component_path, nbs, out_nbs):
     nbs_written = []
     for nb_fname, out_nb_fname in zip(nbs, out_nbs):
@@ -317,7 +307,6 @@ def main():
             continue
         nbs = get_notebooks(component_path, lexts=('.rmd',))
         out_nbs = [pth_maker(f) for f in nbs]
-        write_data(component_path, component.get('data_files', []))
         write_component(component_path, nbs, out_nbs)
         clean_nb_dirs(out_nbs)
         out_nb_path = op.dirname(out_nbs[-1])
